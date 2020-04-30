311f426835413b2184963a2e81122b17241c333f
YanislavTerentev <62559697+YanislavTerentev@users.noreply.github.com>
2020-04-30 13:09:24 +0300

    HW44

--- src/by/academy/HomeWork/HomeWork6/ClassWorkExceptionMain.java
+++ src/by/academy/HomeWork/HomeWork6/ClassWorkExceptionMain.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 
 public class ClassWorkExceptionMain {
     public static void main(String[] args) {
-        System.out.print("Введите любое целое число от 1 до 10: ");
+
         int result;
         int n = 0;
         do {
             try {
                 n++;
+                System.out.println("Введите любое целое число от 1 до 10: ");
                 result = input();
                 if (0 < result && result <= 10) {
                     System.out.println("Введенное число подходит");
@@ -30,6 +31,7 @@ public class ClassWorkExceptionMain {
 
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
+
         if (0 < i && i <= 10) {
             return i;
         } else {
