7fe196428ddda735cfa0d0a669557f65119d6fa1
YanislavTerentev <62559697+YanislavTerentev@users.noreply.github.com>
2020-05-22 10:26:44 +0300

    123

--- src/by/academy/HomeWork/HomeWork6/OutOfMemoryErrorMain.java
+++ src/by/academy/HomeWork/HomeWork6/OutOfMemoryErrorMain.java
@@ -1,15 +1,20 @@
 package by.academy.HomeWork.HomeWork6;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Random;
 
 public class OutOfMemoryErrorMain {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        Random random = new Random();
+        try {
+            List<Integer> list = new ArrayList<>();
+            Random random = new Random();
 
-        while (true)
-            list.add(random.nextInt());
+            while (true)
+                list.add(random.nextInt());
+        }catch (OutOfMemoryError| IllegalArgumentException | InputMismatchException e){
+            System.err.println("You have problem with memory");
+        }
     }
 }
